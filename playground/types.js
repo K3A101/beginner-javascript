11b6c9698aa561d636064df0757706b76020ffaa
K3A101 <alexanderkeisha9@gmail.com>
2025-07-01 10:11:23 +0200

    Types module 1

---
+++ playground/types.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+/* const first = 'Kea';
+const middle = "Ep";
+const last = `Alexander`;
+
+const sentence = 'shes\'s so cool';
+const sentence2 = `shes\'s so "cool"`;
+
+const hello = "hello my name is " + first + ". Nice to meet you!"
+
+const hello2 = `Hello my name is ${first},  I am ${20+20} years old!`;
+
+
+const html = `
+<div>
+<h2>${last}</h2>
+</div>
+`;
+document.body.innerHTML = html */
+
+
+// const age = 100;
+// const name = 'Kea';
+// const a = 100;
+// const b = 290;
+
+// const smarties = 20;
+// const kids = 3;
+
+// const eachKidGets = Math.floor(smarties / kids);
+// const dad = smarties % kids;
+// console.log(`Each kid gets ${eachKidGets}`);
+
+// const person = {
+//     first: 'Keisha',
+//     last: 'Alexander',
+//     age: 23
+// };
+
+let isDrawing = false;
+const age = 18;
+const ofAge = age > 19;
+console.log(ofAge);
+
