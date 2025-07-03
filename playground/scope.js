78628ffbbf68cf12adb0ba6d6e2fa2868b01d952
K3A101 <alexanderkeisha9@gmail.com>
2025-07-03 15:39:35 +0200

    Scope excersise

---
+++ playground/scope.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+const age = 100;
+
+// function go() {
+//     const hair = 'blond';
+//     return hair;
+// }
+
+// let cool;
+// if ( 1 === 1) {
+//  cool = true;
+// } else {
+//     cool = false;
+// }
+
+// console.log(cool);
+
+const dog = 'Snickers';
+ 
+function logDog() {
+    console.log(dog);
+}
+
+function go() {
+    const dog = 'Sunny';
+    logDog();
+}
+
+go()
