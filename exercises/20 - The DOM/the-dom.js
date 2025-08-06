34b4b7042f40f973e9578c56e159af379a178ac5
K3A101 <alexanderkeisha9@gmail.com>
2025-08-06 17:24:31 +0200

    excercise the dom

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,15 @@
+// const pElement = document.querySelector('p');
+
+// const heading = document.querySelector('h2');
+// heading.textContent = 'Yikes on Bikes';
+// heading.innerText = 'Yikes on Bikes';
+// // console.dir(heading);
+
+// const pizzaList = document.querySelector('.pizza');
+// pizzaList.insertAdjacentText('beforeend', '🍕');
+
+// console.log(heading);
+// classes
+const pic = document.querySelector('.nice');
+pic.classList.add('cool');
+console.log(pic.classList);
